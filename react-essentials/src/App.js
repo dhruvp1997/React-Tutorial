a5cddf9ada10a348ddeca70d5d0f5b0396b362d2
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-28 16:58:32 -0400

    Starting of useState

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,25 +1,9 @@
 import './App.css';
 
-function SecretComponent(){
-  return <h1>Secret information for authorized users only</h1>
-}
-
-function RegularComponent(){
-  return <h1>Everyone can see this component</h1>
-}
-
-function App({authorized}) {
-    // props is a list of objects
-    // here authorized is a object
-    /* if(props.authorized){
-      return <SecretComponent />
-    }
-    else{
-      return <RegularComponent />
-    }   Basic conditional rendering*/
+function App() {
     return(
       <>
-      {authorized ? <SecretComponent /> : <RegularComponent />} {/*JSX conditional rendering */}
+      <h1>Hello</h1>
       </>
     )
 }
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App"
 
-const checklist=["boots","tent","headlamp"];
-const [first]=["boots","tent","headlamp"];
-const [important,second]=["boots","tent","headlamp"];
-const [,,light]=["boots","tent","headlamp"];
-console.log(checklist);
-console.log(checklist[1]);
-console.log(first);
-console.log(important,second);
-console.log(light);
-
-
 ReactDOM.render(
-  <App authorized={false} />,
+  <App />,
   document.getElementById('root')
 );
 
