2a2433bf3425f4cb894204b0e3e725e7da1a71df
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-28 14:46:55 -0400

    Implementing JSX

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -4,15 +4,11 @@ import './index.css';
 
 
 ReactDOM.render(
-  React.createElement(
-    "ul",
-     null,
-     React.createElement("li", null,"Monday"),
-     React.createElement("li", null,"Tuesday"),
-     React.createElement("li", null,"Wednesday"),
-     React.createElement("li", null,"Thursday"),
-     React.createElement("li", null,"Friday"),
-     ),
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+  </ul>,
   document.getElementById('root')
 );
 
