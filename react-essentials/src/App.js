41af881c359685ea192b25a7156f381c4c6db02c
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-28 17:26:14 -0400

    Inputbox with checked unchecked states

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,28 +1,12 @@
-import React, {useState,useEffect} from "react";
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy"); // we can pass anything in useState
-  const [secondary, setSecondary] = useState("tired");
-
-  //function(()=>{ }); this is called callback function
-
-useEffect(()=>{ 
-    console.log(`It's ${emotion} around here!`);
-}, [emotion]);
-
-useEffect(()=>{ 
-  console.log(`It's ${secondary} around here!`);
-},[secondary]);
-
+  const [checked, setChecked] = useState(false);
   return(
       <>
-      <h1>Current emotion is {emotion} and {secondary}.</h1>
-      <button onClick={()=> setEmotion("happy")}>Make Happy</button>
-      <button onClick={()=> setSecondary("crabby")}>Make Crabby</button>
-      <button onClick={()=> setSecondary("tired")}>Make Tired</button>
-      <button onClick={()=> setEmotion("frustrated")}>Frustrate</button>
-      <button onClick={()=> setEmotion("enthusiastic")}>Enthusiastic</button>
+        <input type="checkbox" value={checked} onChange={() => setChecked((checked)=> !checked)}/>
+        <p>{checked ? "checked": "not checked"}</p>
       </>
     )
 }
