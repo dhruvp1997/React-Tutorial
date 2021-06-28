8c474a657101895772537075efaabd0aa53bfe47
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-28 16:39:23 -0400

    Removing parts and adding conditional rendering

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,54 +1,20 @@
 import './App.css';
-import restaurant from "./restaurant.jpg"
 
-function Header(props){
-  return(
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
-}
-function Main(props){
-  return(
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      {/* <img src="https://github.com/dhruvp1997.png" height={200}/> */}
-      <img src={restaurant} height={200} alt="Fancy Restaurant"/>
-      <ul style={{textAlign:"left"}}>
-        {
-          // props.dishes.map((dish,i) => <li key={i}>{dish}</li>)  When we use it as string
-          props.dishes.map((dish) => <li key={dish.id}>{dish.title}</li>) //When we pass as object
-        }
-      </ul>
-    </section>
-  );
-}
-function Footer(props){
-  return(
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
+function SecretComponent(){
+  return <h1>Secret information for authorized users only</h1>
 }
 
-const dishes = [
-  "Dosa",
-  "Panipuri",
-  "Idli",
-  "Burger",
-]
+function RegularComponent(){
+  return <h1>Everyone can see this component</h1>
+}
 
-const dishObjects = dishes.map((dish,i)=> ({id: i, title:dish})); //Object of dishes
-
-function App() {
-  return (
-    <div className="App"> {/* we can also wrap over here like index.js */}
-      <Header name="Dimpal" />
-      {/* <Main adjective="amazing" dishes={dishes} /> Using String property */}
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+function App(props) {
+    if(props.authorized){
+      return <SecretComponent />
+    }
+    else{
+      return <RegularComponent />
+    }
 }
 
 export default App;
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,20 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App"
 
-// function AppTwo(){
-//   return <h1>This is second app</h1>
-// }
-
 ReactDOM.render(
-  /*<>
-  <React.Fragment>
-  <div>
-  <App />
-  <AppTwo />
-  </div>
-  </React.Fragment>
-  </>, Wrapping two component into one 3 different ways using div, react.fragment,empty tag*/
-  <App />,
+  <App authorized={true} />,
   document.getElementById('root')
 );
 
