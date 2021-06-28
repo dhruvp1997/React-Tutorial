fc84451db59f625e720d92a8b4aeae5e26095481
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-28 17:19:59 -0400

    Working with useState

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,13 +1,26 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import './App.css';
 
 function App() {
-  const [emotion, setEmotion] = useState("Happy"); // we can pass anything in useState
-  
+  const [emotion, setEmotion] = useState("happy"); // we can pass anything in useState
+  const [secondary, setSecondary] = useState("tired");
+
+  //function(()=>{ }); this is called callback function
+
+useEffect(()=>{ 
+    console.log(`It's ${emotion} around here!`);
+}, [emotion]);
+
+useEffect(()=>{ 
+  console.log(`It's ${secondary} around here!`);
+},[secondary]);
+
   return(
       <>
-      <h1>Current emotion is {emotion}.</h1>
-      <button onClick={()=> setEmotion("happy")}>Happy</button>
+      <h1>Current emotion is {emotion} and {secondary}.</h1>
+      <button onClick={()=> setEmotion("happy")}>Make Happy</button>
+      <button onClick={()=> setSecondary("crabby")}>Make Crabby</button>
+      <button onClick={()=> setSecondary("tired")}>Make Tired</button>
       <button onClick={()=> setEmotion("frustrated")}>Frustrate</button>
       <button onClick={()=> setEmotion("enthusiastic")}>Enthusiastic</button>
       </>
