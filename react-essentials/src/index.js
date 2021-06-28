7a9afdab55f2db18296dcc7571f5508dca33bde2
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-28 14:42:49 -0400

    Unordered List in React.CreateElement

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 
 
 ReactDOM.render(
-  React.createElement("h1", {style: {color:"blue"}},"Heyy Everyone"),
+  React.createElement(
+    "ul",
+     null,
+     React.createElement("li", null,"Monday"),
+     React.createElement("li", null,"Tuesday"),
+     React.createElement("li", null,"Wednesday"),
+     React.createElement("li", null,"Thursday"),
+     React.createElement("li", null,"Friday"),
+     ),
   document.getElementById('root')
 );
 
