77961eff86d547d73ccd10f0e9af5df764ef49e5
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-28 17:28:38 -0400

    Adding toggle function inside App function

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   const [checked, setChecked] = useState(false);
+  
+  function toggle(){
+    setChecked((checked)=> !checked)
+  }
+
   return(
       <>
-        <input type="checkbox" value={checked} onChange={() => setChecked((checked)=> !checked)}/>
+        <input type="checkbox" value={checked} onChange={toggle}/>
         <p>{checked ? "checked": "not checked"}</p>
       </>
     )
