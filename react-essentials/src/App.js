be2b9c2294415393bdeabab07c804aa70cd2e5dc
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-29 17:31:57 -0400

    First file to test with react library

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,42 +1,12 @@
-import React, {useState, useEffect} from "react";
 import './App.css';
 
-// https://api.github.com/users/dhruvp1997
-
-function App({login}) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error,setError] = useState(null);
-
-
-  useEffect(()=> {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-    .then((response)=> response.json())
-    .then(setData)
-    .then(()=> setLoading(false))
-    .catch(setError);
-  },[login]);
-
-  if(loading) return <h1>Loading....</h1>;
-  if(error)
-  return <pre>{JSON.stringify(error,null,2)}</pre>
-  if(!data) return null;
+function App() {
   
     return (
     <div>
-      <h1>{data.name}</h1>
-      <p>{data.location}</p>
-      <img src={data.avatar_url} alt={data.login}/>
+      <h1>Hello React Testing Library</h1>
     </div>);
   
-
-  return(
-      <div>
-        No user available
-      </div>
-    )
 }
 
 export default App;
