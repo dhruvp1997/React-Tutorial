f612c3ab59d9905f155e1b73c75b393152410776
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-07-02 13:51:28 -0400

    Adding more pages and routes

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/HomePage'
-
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
 class App extends Component{
   render(){
   return (
     <Router>
     <div className="App">
+      <div id="page-body">
       <Route path="/" component={HomePage} exact />
+      <Route path="/about" component={AboutPage} exact />
+      <Route path="/articles-list" component={ArticlesList} exact />
+      <Route path="/article" component={ArticlePage} exact />
+      </div>
     </div>
     </Router>
   );
