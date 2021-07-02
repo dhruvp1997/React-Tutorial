96f2a14edcd1c6daacea435acc868f4f641c5ba4
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-07-02 15:34:59 -0400

    Route Parameters in express

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,9 +4,11 @@ import bodyParser from 'body-parser';
 const app = express();
 
 app.use(bodyParser.json());
-
+//simple get request 
 app.get('/hello', (req, res) => res.send('Hello!'));
-
+//get request using url/name or id
+app.get('/hello/:name', (req,res)=> res.send(`Hello ${req.params.name}`));
+//post request with name
 app.post('/hello',(req,res) => res.send(`Hello ${req.body.name}`));
 
 app.listen(8000,()=> console.log('Listening on port 8000'));
