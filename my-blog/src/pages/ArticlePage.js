a74d417b87483c508b2e801de8e5b261ffeb9651
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-07-02 23:39:53 -0400

    Adding Fetch to pages
    Here we have to start 4 things
    1) frontend server
    2) backend server
    3) mongod
    4) mongo

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,33 +1,38 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
-import articleContent from './article-content';
 import NotFoundPage from './NotFoundPage';
+import articleContent from './article-content';
 
-const ArticlePage = ({match}) => {
-    
+const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
-    
-    const [articleInfo, setArticleInfo]= useState({upvotes:0,comments:[]});
 
-    useEffect(()=> {
-        setArticleInfo({upvotes: Math.ceil(Math.random()*10)});
-    },[name]);
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-    if(!article) return <NotFoundPage />
-    
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
+
+    if (!article) return <NotFoundPage />
+
     const otherArticles = articleContent.filter(article => article.name !== name);
 
-    return(
-    <>
-    <h1>{article.title}</h1>
-    <p>This post has been upvoted {articleInfo.upvotes} times</p>
-    {article.content.map((paragraph, key) => (
-        <p key={key}>{paragraph}</p>
-    ))}
-    <h3>Other Articles:</h3>
-    <ArticlesList articles={otherArticles} />
-    </>
+    return (
+        <>
+        <h1>{article.title}</h1>
+        <p>This post has been upvoted {articleInfo.upvotes} times</p>
+        {article.content.map((paragraph, key) => (
+            <p key={key}>{paragraph}</p>
+        ))}
+        <h3>Other Articles:</h3>
+        <ArticlesList articles={otherArticles} />
+        </>
     );
 }
 
