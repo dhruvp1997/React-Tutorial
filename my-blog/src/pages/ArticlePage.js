4dc061c0ba89adfeda1b16d9d9f477e9ca75252c
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-07-02 23:46:16 -0400

    CommentsList in ArticlePage

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
+import CommentsList from '../components/CommentsList';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
@@ -30,6 +31,7 @@ const ArticlePage = ({ match }) => {
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
+        <CommentsList comments={articleInfo.comments} />
         <h3>Other Articles:</h3>
         <ArticlesList articles={otherArticles} />
         </>
