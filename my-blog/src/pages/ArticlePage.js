99eb6f7836dacd8efb01a96faa1493e38de76eb8
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-07-03 00:14:27 -0400

    Adding an Add Comment form

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import CommentsList from '../components/CommentsList';
 import UpvotesSection from '../components/UpvotesSection';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
@@ -32,6 +33,7 @@ const ArticlePage = ({ match }) => {
             <p key={key}>{paragraph}</p>
         ))}
         <CommentsList comments={articleInfo.comments} />
+        <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
         <h3>Other Articles:</h3>
         <ArticlesList articles={otherArticles} />
         </>
